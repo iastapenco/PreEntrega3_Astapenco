5a7702d998579f3fd7de007e963220a1bb11f416
Iván Astapenco <iastapenco@gmail.com>
2023-04-29 09:36:12 -0300

    se da estilo a la lista

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ class Articulo {
 }
 
 const articulos = [
-    new Articulo("pantalón jeans", 2000),
-    new Articulo("pantalón de vestir", 2500),
-    new Articulo("remera", 900),
-    new Articulo("buzo", 1200),
-    new Articulo("camisa", 1600),
-    new Articulo("medias", 300),
+    new Articulo("Pantalón jeans", 2000),
+    new Articulo("Pantalón de vestir", 2500),
+    new Articulo("Remera", 900),
+    new Articulo("Buzo", 1200),
+    new Articulo("Camisa", 1600),
+    new Articulo("Medias", 300),
 ]
 
 const articulosSeleccionados = inicializarArticulosSeleccionados();
@@ -21,6 +21,7 @@ let preciosSinDescuento = 0;
 function inicializarArticulosSeleccionados() {
     const articulosStorage = localStorage.getItem("articulos_seleccionados");
     const articulosParsed = JSON.parse(articulosStorage);
+    if(articulosParsed != null && articulosParsed.length > 2){localStorage.removeItem("articulos_seleccionados")}
     return articulosParsed != null ? articulosParsed : [] ;
     
 };
@@ -34,49 +35,77 @@ function añadirArticulo(x) {
 function obtenerArticulo(nombreArticulo) {
     const articulo1 = articulos.find(articulo => articulo.nombre == nombreArticulo);
     añadirArticulo(articulo1);
+    agregarAListaDeSeleccionados(articulo1)
     const calcularDescuento = articulosSeleccionados.length == 3 ? true : false;
     if (calcularDescuento) {
         obtenerPrecio();
         document.getElementById("modal-calculadora").innerText = `El descuento total será $ ${descuentoTotal} y el pago total $ ${preciosSinDescuento - descuentoTotal}`
         var modal1 = new bootstrap.Modal(document.getElementById('modal'));
         modal1.toggle();
-        localStorage.removeItem("articulos_seleccionados")
+        localStorage.removeItem("articulos_seleccionados");
+        document.getElementById("lista_articulos").innerHTML = "";
     }
 }
 
+function agregarAListaDeSeleccionados(articulo) {
+    const li = document.createElement("li");
+    li.setAttribute("class", "list-group-item d-flex justify-content-between align-items-start");
+    
+    const div = document.createElement("div");
+    div.setAttribute("class", "ms-2 me-auto");
+
+    const divBold = document.createElement("div");
+    divBold.setAttribute("class", "fw-bold");
+
+    divBold.textContent = articulo.nombre;
+    
+    div.appendChild(divBold);
+    div.appendChild(document.createTextNode(`$ ${articulo.precio}`)) ;
+    li.appendChild(div);
+    
+    const ol = document.getElementById("lista_articulos");
+
+    ol.appendChild(li);
+}
+function agregadoCorrectamente(botonId) {
+    document.getElementById(botonId).setAttribute("class", "btn btn-success btn-lg");
+    setTimeout(() => {
+        document.getElementById(botonId).setAttribute("class", "btn btn-primary btn-lg");
+    }, 1000);
+}
 document.getElementById("btn-jeans").addEventListener("click", () => {
-    obtenerArticulo("pantalón jeans");
-    document.getElementById("btn-jeans").setAttribute("class", "btn btn-success btn-lg");
+    obtenerArticulo("Pantalón jeans");
+    agregadoCorrectamente("btn-jeans");
 })
 
 document.getElementById("btn-vestir").addEventListener("click", () => {
-    obtenerArticulo("pantalón de vestir");
-    document.getElementById("btn-vestir").setAttribute("class", "btn btn-success btn-lg");
+    obtenerArticulo("Pantalón de vestir");
+    agregadoCorrectamente("btn-vestir");
 })
 
 document.getElementById("btn-remera").addEventListener("click", () => {
-    obtenerArticulo("remera");
-    document.getElementById("btn-remera").setAttribute("class", "btn btn-success btn-lg");
+    obtenerArticulo("Remera");
+    agregadoCorrectamente("btn-remera");
 })
 
 document.getElementById("btn-buzo").addEventListener("click", () => {
-    obtenerArticulo("buzo");
-    document.getElementById("btn-buzo").setAttribute("class", "btn btn-success btn-lg");
+    obtenerArticulo("Buzo");
+    agregadoCorrectamente("btn-buzo");
 })
 
 document.getElementById("btn-camisa").addEventListener("click", () => {
-    obtenerArticulo("camisa");
-    document.getElementById("btn-camisa").setAttribute("class", "btn btn-success btn-lg");
+    obtenerArticulo("Camisa");
+    agregadoCorrectamente("btn-camisa");
 })
 
 document.getElementById("btn-medias").addEventListener("click", () => {
-    obtenerArticulo("medias");
-    document.getElementById("btn-medias").setAttribute("class", "btn btn-success btn-lg");
+    obtenerArticulo("Medias");
+    agregadoCorrectamente("btn-medias");
 })
 
-
-console.log(articulosSeleccionados);
-
+articulosSeleccionados.forEach(element => {
+    agregarAListaDeSeleccionados(element);
+});
 
 const obtenerPrecio = () => {
     const preciosSeleccionados = articulosSeleccionados.map(art => art.precio);
@@ -96,6 +125,8 @@ const obtenerPrecio = () => {
         preciosSinDescuento += (precio);
     })
 }
+
+
 
 console.log(descuentosFinales);
 
